8bb733e025d6deb560b3279f1294460a2e6f8e4c
bapmito <nmtoan224@gmail.com>
2020-09-23 00:05:33 +0700

    new

--- src/models/materialSetting/formwork/CategoryTypeFilter_FormworkValueBinding_Dictionary.tsx
+++ src/models/materialSetting/formwork/CategoryTypeFilter_FormworkValueBinding_Dictionary.tsx
@@ -1,15 +1,21 @@
 import { List } from '../../list/List';
 import { CategoryTypeFilter_FormworkValueBinding } from './CategoryTypeFilter_FormworkValueBinding';
 
-export class CategoryTypeFilter_FormworkValueBinding_Dictionary {
+export class CategoryTypeFilter_FormworkValueBinding_Dictionary 
+{
   private _categoryTypeFilter_FormworkValueBindings?: List<CategoryTypeFilter_FormworkValueBinding>;
-  public get categoryTypeFilter_FormworkValueBindings() : List<CategoryTypeFilter_FormworkValueBinding>{
-    if (this._categoryTypeFilter_FormworkValueBindings == null){
+  public get categoryTypeFilter_FormworkValueBindings() : List<CategoryTypeFilter_FormworkValueBinding>
+  {
+    if (this._categoryTypeFilter_FormworkValueBindings == null)
+    {
       this._categoryTypeFilter_FormworkValueBindings = new List<CategoryTypeFilter_FormworkValueBinding>();
     }
     return this._categoryTypeFilter_FormworkValueBindings;
   }
-  public set categoryTypeFilter_FormworkValueBindings(value: List<CategoryTypeFilter_FormworkValueBinding>){
-    this._categoryTypeFilter_FormworkValueBindings = value;
+
+  public set categoryTypeFilter_FormworkValueBindings (value: List<CategoryTypeFilter_FormworkValueBinding> )
+  {
+    this._categoryTypeFilter_FormworkValueBindings=value;
+    
   }
 }
